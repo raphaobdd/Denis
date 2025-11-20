e867be3a7bada39ff1e3515475553e23925cb7ba
raphaobdd <raphaelbentovonzuben@gmail.com>
2025-11-20 16:08:26 -0300

    novo

--- backend/app.py
+++ backend/app.py
@@ -1,2 +1,20 @@
-from flask import Flask
+from requests_api import get_all_data
+from repository import salvar_dados_api
+from os import getenv
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+def main():
+    # 1. Puxar dados da API
+    url = os.getenv("API_URL")
+    dados = get_all_data(url)
+
+    # 2. Salvar no Postgres
+    salvar_dados_api(dados)
+
+    print("Dados importados para o Postgres")
+
+if __name__ == "__main__":
+    main()
