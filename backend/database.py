7505add0eb9f360adddb10b91b041a5a70272c04
raphaelvonzuben <102269161+raphaobdd@users.noreply.github.com>
2025-11-21 16:48:31 +0000

    Modelo Treinado

--- backend/database.py
+++ backend/database.py
@@ -1,17 +1,67 @@
-import psycopg2
+from supabase import create_client
+import requests
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-try:
-    DB_URL = os.getenv("DATABASE_URL")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-    if not DB_URL:
-        raise ValueError("Variável SUPABASE_DB_URL não encontrada no .env")
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-    connection = psycopg2.connect(DB_URL, sslmode="require")
-    print("Conexão bem-sucedida!")
+API_URL = os.getenv('API_URL')
+API_KEY = os.getenv('API_KEY')
 
-except Exception as e:
-    print("Erro ao conectar ao banco:", e)
+
+def fetch_asteroids(pages=3):
+    url = API_URL
+    params = {"api_key": API_KEY}
+    results = []
+
+    for _ in range(pages):
+        response = requests.get(url, params=params, timeout=20)
+        response.raise_for_status()
+
+        data = response.json()
+        neos = data.get("near_earth_objects", [])
+
+        for neo in neos:
+            item = {
+                "id": neo["id"],
+                "name": neo["name"],
+                "absolute_magnitude": neo["absolute_magnitude_h"],
+                "is_potentially_hazardous_asteroid": neo["is_potentially_hazardous_asteroid"],
+                "diameter_km_min": neo["estimated_diameter"]["kilometers"]["estimated_diameter_min"],
+                "diameter_km_max": neo["estimated_diameter"]["kilometers"]["estimated_diameter_max"],
+            }
+
+            cad = neo.get("close_approach_data", [])
+            if cad:
+                approach = cad[0]
+                item["close_approach_date"] = approach.get("close_approach_date")
+                item["rel_km_s"] = float(approach["relative_velocity"]["kilometers_per_second"])
+                item["miss_km"] = float(approach["miss_distance"]["kilometers"])
+            else:
+                item["close_approach_date"] = None
+                item["rel_km_s"] = None
+                item["miss_km"] = None
+
+            results.append(item)
+
+        url = data.get("links", {}).get("next")
+        if not url:
+            break
+
+    return results
+
+
+def salvar_asteroides_supabase(lista_dados):
+    for item in lista_dados:
+        supabase.table("asteroids").upsert(item).execute()
+
+
+# Executando todo o fluxo
+dados = fetch_asteroids(pages=3)
+salvar_asteroides_supabase(dados)
+print(f"{len(dados)} asteroides inseridos/atualizados!")
--- backend/requests_api.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def get_all_data(url, params=None):
-    response = requests.get(url, params=params, timeout=30)
-    response.raise_for_status()
-    return response.json()   # retorna lista ou dict
-
